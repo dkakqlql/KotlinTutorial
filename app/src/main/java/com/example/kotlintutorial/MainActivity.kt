d8c4610e4d233adff79be6a4ad5c1cf14976b80c
kmh <dkakqlql@gmail.com>
2022-01-18 17:40:45 +0900

    ProtoType 2nd(onboarding slider)

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -1,11 +1,120 @@
 package com.example.kotlintutorial
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.button.MaterialButton
+import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var onboardingItemsAdaptor: OnboardingItemsAdaptor
+    private lateinit var indicatorsContainer: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MyApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setOnboardingItems()
+        setupIndicators()
+        setCurrentIndicator(0)
+    }
+
+    private fun setOnboardingItems() {
+        onboardingItemsAdaptor = OnboardingItemsAdaptor(
+            listOf(
+                OnboardingItem(
+                    onboardingImage = R.drawable.ic_launcher_foreground,
+                    title = "First Page",
+                    description = "This is the first page of the app. Please go to next page through dragging."
+                ),
+                OnboardingItem(
+                    onboardingImage = R.drawable.ic_right,
+                    title = "Second Page",
+                    description = "This is the second page of the app. follow this script slowly."
+                ),
+                OnboardingItem(
+                    onboardingImage = R.drawable.ic_right,
+                    title = "Last Page",
+                    description = "This is the last page of onboarding screens. Please click below button."
+                )
+            )
+        )
+        val onboardingViewPager = findViewById<ViewPager2>(R.id.onboardingViewPager)
+        onboardingViewPager.adapter = onboardingItemsAdaptor
+        onboardingViewPager.registerOnPageChangeCallback(object :
+        ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+        (onboardingViewPager.getChildAt(0) as RecyclerView).overScrollMode =
+            RecyclerView.OVER_SCROLL_NEVER
+        findViewById<ImageView>(R.id.imageNext).setOnClickListener {
+            if (onboardingViewPager.currentItem + 1 < onboardingItemsAdaptor.itemCount) {
+                onboardingViewPager.currentItem += 1
+            } else {
+                navigateToHomeActivity()
+            }
+        }
+        findViewById<TextView>(R.id.textSkip).setOnClickListener { navigateToHomeActivity() }
+        findViewById<MaterialButton>(R.id.btn_get_started).setOnClickListener { navigateToHomeActivity() }
+    }
+
+    private fun navigateToHomeActivity() {
+        startActivity(Intent(applicationContext, HomeActivity::class.java))
+        finish()
+    }
+
+    private fun setupIndicators() {
+        indicatorsContainer = findViewById(R.id.indicatorsContainer)
+        val indicators = arrayOfNulls<ImageView>(onboardingItemsAdaptor.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8,0,8,0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let {
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+                it.layoutParams = layoutParams
+                indicatorsContainer.addView(it)
+            }
+        }
+    }
+
+    private fun setCurrentIndicator(position: Int) {
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainer.getChildAt(i) as ImageView
+            if(i == position) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+            }
+        }
     }
 }
